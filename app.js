b11c951cfd4ea184dfac757563c2735ad64750e7
Nebil.Nurhussien@city.ac.uk <106036675+nnurhussien04@users.noreply.github.com>
2025-08-07 00:02:15 +0100

    Tweak layout and sizing for improved design alignment

--- app.js
+++ app.js
@@ -5,14 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
     //const apiKey = 'YOUR_API_KEY';
 
 
-    for(const image of images){
+    /*for(const image of images){
         fetch("https://dog.ceo/api/breeds/image/random").then(response => response.json()).then(data => { 
             image.src = data.message
             image.width = 100;
             image.height = 100;
-    });
-        
+    });        
+    }*/
 
-    }
+    const toggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    toggle.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    });
+
 
 } )
